dc5770a6828fb733005fbe0daee2fbba0414855c
Lukas Taegert <lukas.taegert@tngtech.com>
2017-06-18 21:37:31 +0200

    After first demo

--- demo1/build.js
+++ demo1/build.js
@@ -1,12 +1,10 @@
-require('rollup')
-  .rollup({entry: 'src/index.js'})
-  .then(bundle => {
-    bundle.write({
-      dest: 'dist/index.js',
-      format: 'cjs' // CommonJS module
-    })
-    bundle.write({
-      dest: 'dist/index.mjs',
-      format: 'es' // ES6 module
-    })
+const rollup = require('rollup')
+
+rollup
+  .rollup({
+    entry: 'src/index.js'
   })
+  .then(bundle => bundle.write({
+    dest: 'dist/index.js',
+    format: 'cjs'
+  }))
